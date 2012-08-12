b27348749f46249718203a2cd4b5df387dbd28e0
osonmez <menelaus_1988@hotmail.com>
2012-08-13 00:21:00 +0300

    first maven commit

---
+++ Demo/src/main/java/com/osonmez/demo/App.java
@@ -0,0 +1,21 @@
+package com.osonmez.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public int getNumber1(){
+    	return 1;
+    }
+    
+    public int getNumber2(){
+    	return 2;
+    }
+}
